6b1071a3f1f7a2ac50eacd8e77b24174b249cdaa
gomezv24 <vagomez@estudiantec.cr>
2025-04-14 00:13:48 -0600

    Random endpoint

--- project1/main.go
+++ project1/main.go
@@ -1,60 +1,63 @@
 package main
 
 import (
-    // "fmt"
-    "log"
-    "net"
-    // "strings"
-    "http-servidor/handlers"
-    "http-servidor/utils"
+	// "fmt"
+	"log"
+	"net"
+
+	// "strings"
+	"http-servidor/handlers"
+	"http-servidor/utils"
 )
 
 const PORT = ":8080"
 
 func main() {
-    ln, err := net.Listen("tcp", PORT)
-    if err != nil {
-        log.Fatalf("Error al iniciar servidor: %v", err)
-    }
-    defer ln.Close()
-    log.Printf("Servidor escuchando en %s", PORT)
+	ln, err := net.Listen("tcp", PORT)
+	if err != nil {
+		log.Fatalf("Error al iniciar servidor: %v", err)
+	}
+	defer ln.Close()
+	log.Printf("Servidor escuchando en %s", PORT)
 
-    for {
-        conn, err := ln.Accept()
-        if err != nil {
-            log.Printf("Error aceptando conexión: %v", err)
-            continue
-        }
-        go handleConnection(conn)
-    }
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("Error aceptando conexión: %v", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
 }
 
 func handleConnection(conn net.Conn) {
-    defer conn.Close()
+	defer conn.Close()
 
-    buffer := make([]byte, 1024)
-    n, err := conn.Read(buffer)
-    if err != nil {
-        log.Printf("Error leyendo: %v", err)
-        return
-    }
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		log.Printf("Error leyendo: %v", err)
+		return
+	}
 
-    request := string(buffer[:n])
-    method, path := utils.ParseRequestLine(request)
+	request := string(buffer[:n])
+	method, path := utils.ParseRequestLine(request)
 
-    if method != "GET" {
-        utils.SendResponse(conn, "405 Method Not Allowed", "Solo se permite GET")
-        return
-    }
+	if method != "GET" {
+		utils.SendResponse(conn, "405 Method Not Allowed", "Solo se permite GET")
+		return
+	}
 
-    route, _ := utils.ParseRoute(path)
+	route, params := utils.ParseRoute(path)
 
-    switch route {
-    case "/help":
-        handlers.Help(conn)
-    case "/timestamp":
-        handlers.Timestamp(conn)
-    default:
-        utils.SendResponse(conn, "404 Not Found", "Ruta no encontrada")
-    }
+	switch route {
+	case "/help":
+		handlers.Help(conn)
+	case "/timestamp":
+		handlers.Timestamp(conn)
+	case "/random":
+		handlers.Random(conn, params["min"], params["max"], params["count"])
+	default:
+		utils.SendResponse(conn, "404 Not Found", "Ruta no encontrada")
+	}
 }
