29d814f24c1004162d9ab44dd78b7b983eacf25b
John Sanchez <jostsace@estudiantec.cr>
2025-06-21 10:57:00 -0600

    fixed args timestamp

--- project1/server/handlers.go
+++ project1/server/handlers.go
@@ -12,38 +12,38 @@ func HandleRequest(req Request) {
 		handlers.Help(req.Conn)
 
 	case "/timestamp":
-		handlers.Timestamp(req.Conn)
+		handlers.Timestamp(req.Conn, utils.SendResponse)
 
 	case "/fibonacci":
 		print("Fibonacci request received\n")
-		handlers.Fibonacci(req.Conn, req.Parametros)
+		handlers.Fibonacci(req.Conn, req.Parametros, utils.SendResponse)
 
 	case "/createfile":
-		handlers.CreateFile(req.Conn, req.Parametros)
+		handlers.CreateFile(req.Conn, req.Parametros, utils.SendResponse)
 
 	case "/deletefile":
-		handlers.DeleteFile(req.Conn, req.Parametros)
+		handlers.DeleteFile(req.Conn, req.Parametros, utils.SendResponse)
 
 	case "/reverse":
-		handlers.Reverse(req.Conn, req.Parametros)
+		handlers.Reverse(req.Conn, req.Parametros, utils.SendResponse)
 
 	case "/toupper":
-		handlers.ToUpper(req.Conn, req.Parametros)
+		handlers.ToUpper(req.Conn, req.Parametros, utils.SendResponse)
 
 	case "/random":
 		handlers.Random(req.Conn, req.Parametros["min"], req.Parametros["max"], req.Parametros["count"], utils.SendResponse)
 
 	case "/hash":
-		handlers.Hash(req.Conn, req.Parametros["text"])
+		handlers.Hash(req.Conn, req.Parametros["text"], utils.SendResponse)
 
 	case "/simulate":
 		handlers.Simulate(req.Conn, req.Parametros["seconds"], req.Parametros["task"])
 
 	case "/sleep":
-		handlers.Sleep(req.Conn, req.Parametros["seconds"])
+		handlers.Sleep(req.Conn, req.Parametros["seconds"], utils.SendResponse)
 
 	case "/loadtest":
-		handlers.Loadtest(req.Conn, req.Parametros["tasks"], req.Parametros["sleep"])
+		handlers.Loadtest(req.Conn, req.Parametros["tasks"], req.Parametros["sleep"], utils.SendResponse)
 
 	case "/ping":
 		handlers.HandlePing(req.Conn)
